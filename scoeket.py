1f9d7fdacd7e582217532b7a28d57badd594213d
Dynix7 <88906210+Dynix7@users.noreply.github.com>
2025-06-27 22:43:58 -0400

    Create scoeket.py

---
+++ scoeket.py
@@ -0,0 +1,33 @@
+import socket as Socket
+import time
+import sys
+import pygame
+import threading
+
+socket = Socket.socket(Socket.AF_INET, Socket.SOCK_STREAM)
+print("socketmake")
+Host = "0.0.0.0"
+Port = 55555
+num = 0
+
+socket.bind((Host, Port))
+print("socket bind")
+socket.listen(5)
+print("waiting")
+conn, adress = socket.accept()
+
+print("connected by: " + adress[0])
+
+while True:
+        give = "amongus"+ str(num)
+        final = give.encode()
+        conn.send(final)
+
+        respond = conn.recv(1024)
+        print(respond)
+        num +=1
+        time.sleep(0.1)
+        
+        
+
+
