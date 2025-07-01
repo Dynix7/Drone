55c7ffc52b9603cd8d362c42b97c769cc74e90c0
Dynix7 <88906210+Dynix7@users.noreply.github.com>
2025-07-01 17:03:59 -0400

    Added Movement and Infinitely Repeating Background

--- Controller.py
+++ Controller.py
@@ -5,20 +5,20 @@ import pygame
 import threading
 import numpy
 
-#Communication variables
-respond = "deez"
+#Communication variables 
+respond = "Data stuff"
 
 #Pygame Variables
 running = True
 
 
-#Mixed
+#movement thingys
 fwd = False
 right = False
 left = False
-Down = False
-Lower = False
-Up = False
+down = False
+lower = False
+up = False
 
 
 def UI():
@@ -26,35 +26,105 @@ def UI():
         #Variables and Stuff
         clock = pygame.time.Clock()
         x,y = (500,500)
-        Font = pygame.font.Font("Font.ttf", 23)
-        textSurface = Font.render(str(respond), True, "Black")
 
-
-        global running
-        #sets the running variable global so u can change
-        
+        #Setup Window/screen 
         screen = pygame.display.set_mode((x,y))
         pygame.display.set_caption("Drone Controller")
-        #Starts and creates the window
+
+        #Load Background
+        Grid = pygame.image.load("Grid.png").convert()
+        GridRect = Grid.get_rect(center = (x/2, y/2))
+
+        #Load Text
+        Font = pygame.font.Font("Font.ttf", 23)
+        textSurface = Font.render(str(respond), True, "White")
+
+        global running, fwd, right, left, down, lower, up
+        #set variables global so u can change
 
         while running:
-                screen.fill((255,255,255))
+
+                #Moves the background by doing opposite of movement key
+                if fwd == True:
+                        GridRect.centery += 3.7
+                if down == True:
+                        GridRect.centery -= 3.7
+                if right == True:
+                        GridRect.centerx -= 3.7
+                if left == True:
+                        GridRect.centerx += 3.7
+
+                
+                #Infinitely repeats grid by teleporting it back to where it came from
+                #Teleport up(fwd)
+                if GridRect.top > 0:
+                        GridRect.centery -=y
+
+                #Teleport Down(down)
+                if GridRect.bottom < 513:
+                        GridRect.centery +=y
+
+                #Teleport Right(Right)
+                if GridRect.right < 500:
+                        GridRect.centerx +=x
+                #Teleport Left(left)
+                if GridRect.left > 0:
+                        GridRect.centerx -=x
+
+
+
+                screen.blit(Grid, GridRect)
+
                 for event in pygame.event.get():
+                        #Closes the window
                         if event.type == pygame.QUIT:
                                 running = False
                                 pygame.quit()
                                 sys.exit()
-                        #Closes the window
+                        
+                        #Movement
+                        if event.type == pygame.KEYDOWN:
+                                
+                                #Fwd movement
+                                if event.key == pygame.K_w:
+                                        fwd = True
 
-                pygame.draw.circle(screen, (0,0,0), (x/2, y/2), 15)
+                                #Down movement        
+                                if event.key == pygame.K_s:
+                                        down = True
+
+                                #Right movement
+                                if event.key == pygame.K_d:
+                                        right = True
+
+                                #Left movement
+                                if event.key == pygame.K_a:
+                                        left = True
+
+
+                        elif event.type == pygame.KEYUP:
+                                #Detects fwd release
+                                if event.key == pygame.K_w:
+                                        fwd = False
+
+                                #Detects down button release thingy idk 
+                                if event.key == pygame.K_s:
+                                        down = False
+
+                                if event.key == pygame.K_d:
+                                        right = False
+
+                                if event.key == pygame.K_a:
+                                        left = False   
+
+                #Draws Circle and text
+                pygame.draw.circle(screen, (255,255,255), (x/2, y/2), 15)
                 screen.blit(textSurface, (0,0))
 
-
+                #Updates the window
                 pygame.display.flip()
                 clock.tick(100)
-                #Updates the window
-                
-
+                   
 
 UI()
 
